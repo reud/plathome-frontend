eb526a728d0d2c95c6f5e10b5c19bf8b9b3265c1
reud <reiji.kobayashi.9n@stu.hosei.ac.jp>
2019-08-17 05:50:39 +0900

    なんとなくレベルの方針決定

--- PlatHome/pages/detail.vue
+++ PlatHome/pages/detail.vue
@@ -1,8 +1,73 @@
 <template>
-  <div>
-    <h2>Welcome {{ deviceData.ipAddress }}</h2>
-    <p>{{ deviceData.hostname }}</p>
-  </div>
+  <v-layout row>
+    <v-flex>
+      <v-card>
+        <v-img
+          src="https://cdn.vuetifyjs.com/images/lists/ali.png"
+          height="300px"
+        >
+          <v-layout column fill-height>
+            <v-card-title>
+              <v-btn dark icon>
+                <v-icon>chevron_left</v-icon>
+              </v-btn>
+
+              <v-spacer></v-spacer>
+
+              <v-btn dark icon class="mr-3">
+                <v-icon>edit</v-icon>
+              </v-btn>
+
+              <v-btn dark icon>
+                <v-icon>more_vert</v-icon>
+              </v-btn>
+            </v-card-title>
+
+            <v-spacer></v-spacer>
+
+            <v-card-title class="white--text pl-5 pt-5">
+              <div class="display-1 pl-5 pt-5">
+                {{ deviceData.hostname }} ({{ deviceData.ipAddress }})
+              </div>
+              <v-chip color="green" text-color="white">Alive</v-chip>
+            </v-card-title>
+          </v-layout>
+        </v-img>
+
+        <v-list two-line>
+          <v-list-tile>
+            <v-list-tile-action>
+              <v-icon color="indigo">phone</v-icon>
+            </v-list-tile-action>
+
+            <v-list-tile-content>
+              <v-list-tile-title>(650) 555-1234</v-list-tile-title>
+              <v-list-tile-sub-title>Mobile</v-list-tile-sub-title>
+            </v-list-tile-content>
+
+            <v-list-tile-action>
+              <v-icon>chat</v-icon>
+            </v-list-tile-action>
+          </v-list-tile>
+
+          <v-list-tile>
+            <v-list-tile-action></v-list-tile-action>
+
+            <v-list-tile-content>
+              <v-list-tile-title>(323) 555-6789</v-list-tile-title>
+              <v-list-tile-sub-title>Work</v-list-tile-sub-title>
+            </v-list-tile-content>
+
+            <v-list-tile-action>
+              <v-icon>chat</v-icon>
+            </v-list-tile-action>
+          </v-list-tile>
+
+          <v-divider inset></v-divider>
+        </v-list>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script lang="ts">
